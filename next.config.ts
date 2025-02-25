6b26a91febdc48b13c25cfc0693b076af546087b
chihabMe <chihab.mg.me@gmail.com>
2025-02-25 16:32:56 +0100

    created a basic structure

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
+
+    ],
+  },
+};
+
+export default nextConfig;
